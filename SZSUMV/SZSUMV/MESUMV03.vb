373732ebacf6720c67f9fba6ddc2dfdc35dd0929
Administrator <Administrator@W7P324JOE>
2018-02-27 14:10:15 +0800

    v1.02

--- SZSUMV/SZSUMV/MESUMV03.vb
+++ SZSUMV/SZSUMV/MESUMV03.vb
@@ -1,4 +1,5 @@
 ﻿Public Class MESUMV03
+    Imports Oracle.ManagedDataAccess.Client
     Private Sub MESUMV03_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Dim oraCon As New OracleConnection()
 
